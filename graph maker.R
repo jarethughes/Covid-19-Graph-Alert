64780932fd6324b7c90b5f6d6d4d319450d680b0
Jaret Hughes <jaret.hughes13@gmail.com>
2020-07-12 14:14:46 -0400

    Iterating Data Import
    Initial work on iterating the data import from JHCSSE. Adding this because I realized graph creation would require time series data in each location, which one day's file wouldn't have.
    
    If there is an easier way to do this and keep the "most up to date data" functionality, please inform me.

--- graph maker.R
+++ graph maker.R
@@ -4,11 +4,34 @@
 #Setup
 library(httr)
 setwd("C:/Users/Jaret/Documents/GitHub/Covid-19-Graph-Alert")
-# name will vary depending on your setup, since this is running on my computer I am using this for now
+#Name will vary depending on your setup, since this is running on my computer I am using this for now
+
+yesterday = Sys.Date()-1
+first.file = as.Date("2020-01-22", "%Y-%m-%d")
+days.since.jan22 = yesterday-first.file
+rdat = data.frame()
+
+importCSV = function(date){
+  csvname = format(date, "%m-%d-%Y")
+  today.data = paste("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/", csvname, ".csv", sep="")
+  rdat = rdat + read.csv(today.data)
+  return()
+}
+
+for (i in 1:days.since.jan22) {
+  importCSV(i)
+}
 
 #Get yesterday's data from John Hopkins CSSE
-date = Sys.Date()-1
-csvname = format(date, "%m-%d-%Y")
+csvname = format(yesterday, "%m-%d-%Y")
 today.data = paste("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/", csvname, ".csv", sep="")
 rdat = read.csv(today.data)
 
+#Clean data
+rdat = subset(rdat, Active>=0)
+#Will add to this as I find errors in the data. 
+
+#Summary
+summary(rdat)
+
+#Graph Making
